c628af8e161b5c71abbc074523d6fcd500c0745b
dkhayutin <dan.khayutin@gmail.com>
2018-05-14 16:23:42 +0000

    Get HTML with callbacks

---
+++ getHTML.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+https.get(options, function(response) {
+
+  response.setEncoding('utf8');
+  response.on('data', function (html) {
+  console.log('Data received. Length:')
+  callback(html)
+
+  var newData = []
+  newData += html;
+  console.log(html )
+  });
+  });
+}
+
+function printHTML (html) {
+  console.log(html)
+}
+  var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step4.html'
+    };
+getHTML(requestOptions, printHTML)
